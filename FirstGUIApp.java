263f7acf307d20ed02ea10320bb1afc6f47236e0
Vaibhav Shukla <vaibhavshukla1967@gmail.com>
2022-10-29 11:52:13 +0530

    First java GUI program

---
+++ FirstGUIApp.java
@@ -0,0 +1,13 @@
+package java_programs;
+
+import javax.swing.JOptionPane;
+
+public class FirstGUIApp {
+    public static void main(String[] ags) {
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        int age = Integer.parseInt((JOptionPane.showInputDialog("Enter your age: ")));
+        String stream = JOptionPane.showInputDialog("Enter your stream: ");
+
+        JOptionPane.showMessageDialog(null, "Hi " + name + ", you are " + age + " years old and you are studying " + stream + ".");
+    }
+}
